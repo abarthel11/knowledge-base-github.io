e64da05ccc90d549b0281cd816e0b6bb4aaa0f24
abarthel11 <austinbarthel@gmail.com>
2022-08-24 17:08:54 -0600

    formQuery function

--- src/utils/queries.js
+++ src/utils/queries.js
@@ -1,1 +1,7 @@
 export const GET_ALL_ARTICLES = `SELECT Title FROM Knowledge__kav WHERE PublishStatus = 'online'  AND Language = 'en_US' WITH DATA CATEGORY Article_Categories__c ABOVE_OR_BELOW All__c`;
+
+export function formQuery(fields, objectApi, conditions){
+    let query = `SELECT ${fields.join(',')} FROM ${objectApi}`
+    //TODO add where conditions
+    return query
+}
